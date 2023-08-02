ed9d1c7fbef3b9899aa9ef0118ce4c9727c3d381
jaesung <ppaccomy@gmail.com>
2023-08-02 18:05:34 +0900

    230802 | 기본UI작업 - 텍스트필드 디자인

---
+++ lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,53 @@
+import 'package:baemin/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool  obscureText;
+  final bool  autoFocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autoFocus = false,
+    required this.onChanged,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0,
+      ),
+    );
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      obscureText: obscureText,  //비밀번호 입력할때
+      autofocus: autoFocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14.0,
+        ),
+        fillColor: INPUT_BG_COLOR,
+        filled: true,
+        border: baseBorder,
+        //기본상태의 스타일
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          ),
+        ),
+      ),
+    );
+  }
+}
