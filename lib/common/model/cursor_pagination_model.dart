8aaa82bea9ec5a098fb8c40f8a1ee3a2f16ee6b4
jaesung <ppaccomy@gmail.com>
2023-08-25 14:28:54 +0900

    230817 | 섹션9-7 Restaurant Pagination - 4

--- lib/common/model/cursor_pagination_model.dart
+++ lib/common/model/cursor_pagination_model.dart
@@ -2,24 +2,25 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
-abstract class CursorPaginationBase{} //상태를 클래스로 만든다.
+abstract class CursorPaginationBase {} //상태를 클래스로 만든다.
 
-class CursorPaginationError extends CursorPaginationBase{ //에러가 났을대의 클래스
+class CursorPaginationError extends CursorPaginationBase {
+  //에러가 났을대의 클래스
   final String message;
 
   CursorPaginationError({
     required this.message,
-});
+  });
 }
 
-
-
-class CursorPaginationLoading extends CursorPaginationBase{}  //로딩중일때의 클래스
+class CursorPaginationLoading extends CursorPaginationBase {} //로딩중일때의 클래스
 
 @JsonSerializable(
-  genericArgumentFactories: true, // 이 옵션을 켜놔야 제네릭으로 들어온 타입의 factory constructor를 사용하여 만들어준다.
+  genericArgumentFactories:
+      true, // 이 옵션을 켜놔야 제네릭으로 들어온 타입의 factory constructor를 사용하여 만들어준다.
 )
-class CursorPagination<T> extends CursorPaginationBase{       //제네릭으로 타입을 넣어줌으로써 아래 data 가 제네릭으로 입력된 클래스로 만들어짐
+class CursorPagination<T> extends CursorPaginationBase {
+  //제네릭으로 타입을 넣어줌으로써 아래 data 가 제네릭으로 입력된 클래스로 만들어짐
   final CursorPaginationMeta meta;
   final List<T> data;
 
@@ -28,8 +29,19 @@ class CursorPagination<T> extends CursorPaginationBase{       //제네릭으로 
     required this.data,
   });
 
+  CursorPagination copyWith({
+    CursorPaginationMeta? meta,
+    List<T>? data,
+  }) {
+    return CursorPagination(
+      meta: meta ?? this.meta,
+      data: data ?? this.data,
+    );
+  }
+
   factory CursorPagination.fromJson(
-          Map<String, dynamic> json, T Function(Object? json) fromJsonT) =>   // T라는 제너릭을 T의 factory constructor를 사용하여 json으로 만들어준다.
+          Map<String, dynamic> json, T Function(Object? json) fromJsonT) =>
+      // T라는 제너릭을 T의 factory constructor를 사용하여 json으로 만들어준다.
       _$CursorPaginationFromJson(json, fromJsonT);
 }
 
@@ -43,18 +55,30 @@ class CursorPaginationMeta {
     required this.hasMore,
   });
 
+  CursorPaginationMeta copyWith({
+    int? count,
+    bool? hasMore,
+  }) {
+    return CursorPaginationMeta(
+      count: count ?? this.count,
+      hasMore: hasMore ?? this.hasMore,
+    );
+  }
+
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
 }
 
-class CursorPaginationRefetching<T> extends CursorPagination<T>{  //다시 처음부터 불러오기 = 새로고침 상태를 나타내는 클래스
+class CursorPaginationRefetching<T> extends CursorPagination<T> {
+  //다시 처음부터 불러오기 = 새로고침 상태를 나타내는 클래스
   CursorPaginationRefetching({
     required super.meta,
     required super.data,
   });
 }
 
-class CursorPaginationFetchingMore<T> extends CursorPagination<T>{  //페이지네이션 다음 데이터를 요청할대의 상태를 나타내는 클래스
+class CursorPaginationFetchingMore<T> extends CursorPagination<T> {
+  //페이지네이션 다음 데이터를 요청할대의 상태를 나타내는 클래스
   CursorPaginationFetchingMore({
     required super.meta,
     required super.data,
--- lib/restaurant/provider/restaurant_provider.dart
+++ lib/restaurant/provider/restaurant_provider.dart
@@ -1,28 +1,31 @@
 import 'package:baemin/common/model/cursor_pagination_model.dart';
+import 'package:baemin/common/model/pagination_params.dart';
 import 'package:baemin/restaurant/repository/restaurant_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final restaurantProvider = StateNotifierProvider<RestaurantStateNotifier,
-    CursorPaginationBase>((ref) {
+final restaurantProvider =
+    StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>((ref) {
   final repository = ref.watch(restaurantRepositoryProvider);
   final notifier = RestaurantStateNotifier(repository: repository);
   return notifier;
 });
 
-class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> { //base를 제네릭으로 넣어주면 extends된 어떤 클래스도 사용가능하다.
+class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
+  //base를 제네릭으로 넣어주면 extends된 어떤 클래스도 사용가능하다.
   final RestaurantRepository repository;
 
   RestaurantStateNotifier({
     required this.repository,
-  }) : super(CursorPaginationLoading()) { //생성단계이므로 로딩인 상태의 클래스를 받도록 한다.
+  }) : super(CursorPaginationLoading()) {
+    //생성단계이므로 로딩인 상태의 클래스를 받도록 한다.
     paginate(); //constructor에 추가하여 바로 실행되도록
   }
 
   void paginate({
     int fetchCount = 20,
     bool fetchMore = false, //true 추가 데이터 가져오기, false 새로고침(현재 상태를 덮어씌움)
-    bool forceRefetch = false,  //강제로 다시 로딩하기 true = CursorPaginationLoading()
-}) async {
+    bool forceRefetch = false, //강제로 다시 로딩하기 true = CursorPaginationLoading()
+  }) async {
     final resp = await repository.paginate();
     state = resp;
     // State의 상태
@@ -36,20 +39,53 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> { //ba
     // 1) hasMore = false (기존 상태에서 이미 다음 데이터가 없다는 값을 들고있다면)
     // 2) 로딩중 - fetchMore = true
     //    fetchMore가 아닐때 - 새로고침의 의도가 있다.
-    if(state is CursorPagination && !forceRefetch){
-      final pState = state as CursorPagination; //캐스팅을 해줌 캐스팅 하기 전에는 state가 CursorPagiantion인줄은 모르므로 강제적으로 변환해준다. 반드시 그 클래스라고 확정적일 경우에만 사용하기
-      if(!pState.meta.hasMore){
+    if (state is CursorPagination && !forceRefetch) {
+      final pState = state
+          as CursorPagination; //캐스팅을 해줌 캐스팅 하기 전에는 state가 CursorPagiantion인줄은 모르므로 강제적으로 변환해준다. 반드시 그 클래스라고 확정적일 경우에만 사용하기
+      if (!pState.meta.hasMore) {
         return;
       }
     }
-    
+
     final isLoading = state is CursorPaginationLoading;
     final isRefetching = state is CursorPaginationRefetching;
     final isFetchingMore = state is CursorPaginationFetchingMore;
-    
+
     // 2)번 상황
-    if(fetchMore && (isLoading || isRefetching || isFetchingMore)){
+    if (fetchMore && (isLoading || isRefetching || isFetchingMore)) {
       return;
+    }
+
+    // PaginationParams 생성
+    PaginationParams paginationParams = PaginationParams(
+      count: fetchCount,
+    );
+    // fetchingMore
+    if (fetchMore) {
+      final pState = state as CursorPagination;
+      state = CursorPaginationFetchingMore(
+        meta: pState.meta,
+        data: pState.data,
+      );
+
+      paginationParams.copyWith(
+        after: pState.data.last.id,
+      );
+
+      final resp = await repository.paginate(
+        paginationParams: paginationParams,
+      );
+
+      if(state is CursorPaginationFetchingMore){
+        final pState = state as CursorPaginationFetchingMore;
+
+        state = resp.copyWith(
+          data: [ //기존 데이터 + 새롱누 데이터 = pState + resp
+            ...pState.data,
+            ...resp.data,
+          ],
+        );
+      }
     }
   }
 }
