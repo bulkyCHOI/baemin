e567d71b08fc8c262c3ca3cef2eca66eaa4ccefd
jaesung <ppaccomy@gmail.com>
2023-08-28 14:21:26 +0900

    230828 | 섹션11-6 RestaurantRatingProvider 초기작업

---
+++ lib/restaurant/provider/restaurant_rating_provider.dart
@@ -0,0 +1,15 @@
+import 'package:baemin/common/model/cursor_pagination_model.dart';
+import 'package:baemin/restaurant/repository/restaurant_rating_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class RestaurantRatingStateNotifier
+    extends StateNotifier<CursorPaginationBase> {
+  final RestaurantRatingRepository repository;
+
+  RestaurantRatingStateNotifier({
+    required this.repository,
+})
+      : super(
+          CursorPaginationLoading(),
+        );
+}
