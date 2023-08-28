ecaa822b85a9fe41cb03e09e26d715c72565e39f
jaesung <ppaccomy@gmail.com>
2023-08-28 13:44:53 +0900

    230828 | 섹션11-4 RatingModel 작업하기

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,7 +1,11 @@
 import 'package:baemin/common/const/data.dart';
 
 class DataUtils{
-  static pathToUrl(String value){   //무조건 static이어야만 적용이 된다.
+  static String pathToUrl(String value){   //무조건 static이어야만 적용이 된다.
     return 'http://$ip$value';
+  }
+
+  static List<String> listPathsToUrls(List<String> paths){
+    return paths.map((e) => pathToUrl(e)).toList();
   }
 }
