2ef428036978cdc94dda71062aa9453bebfb6476
jaesung <ppaccomy@gmail.com>
2023-09-06 16:55:29 +0900

    230906 | 섹션15-3 Get Me API 작업하기

---
+++ lib/user/provider/user_me_provider.dart
@@ -0,0 +1,28 @@
+import 'package:baemin/common/const/data.dart';
+import 'package:baemin/user/model/user_model.dart';
+import 'package:baemin/user/repository/user_me_repository.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:riverpod/riverpod.dart';
+
+class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
+  final UserMeRepository repository;
+  final FlutterSecureStorage storage;
+
+  UserMeStateNotifier({
+    required this.repository,
+    required this.storage,
+  }) : super(UserModelLoading()) {
+    getMe();
+  }
+
+  Future<void> getMe() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    if(refreshToken == null || accessToken == null){
+      return;
+    }
+    final resp = await repository.getMe();
+    state = resp;
+  }
+}
