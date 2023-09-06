4ad8b605f6a62ecd2c775071f0f8e7fd7a214731
jaesung <ppaccomy@gmail.com>
2023-09-06 17:33:03 +0900

    230906 | 섹션15-6 UserMeProvider 마무리하기

--- lib/user/provider/user_me_provider.dart
+++ lib/user/provider/user_me_provider.dart
@@ -1,14 +1,17 @@
 import 'package:baemin/common/const/data.dart';
 import 'package:baemin/user/model/user_model.dart';
+import 'package:baemin/user/repository/auth_repository.dart';
 import 'package:baemin/user/repository/user_me_repository.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:riverpod/riverpod.dart';
 
 class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
+  final AuthRepository authRepository;
   final UserMeRepository repository;
   final FlutterSecureStorage storage;
 
   UserMeStateNotifier({
+    required this.authRepository,
     required this.repository,
     required this.storage,
   }) : super(UserModelLoading()) {
@@ -19,11 +22,48 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
     final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
     final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
 
-    if(refreshToken == null || accessToken == null){
+    if (refreshToken == null || accessToken == null) {
       state = null;
       return;
     }
     final resp = await repository.getMe();
     state = resp;
+  }
+
+  Future<UserModelBase> login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      state = UserModelLoading();
+      final resp = await authRepository.login(
+        username: username,
+        password: password,
+      );
+
+      await storage.write(key: REFRESH_TOKEN_KEY, value: resp.refreshToken);
+      await storage.write(key: ACCESS_TOKEN_KEY, value: resp.accessToken);
+
+      final userResp = await repository.getMe();
+
+      state = userResp;
+
+      return userResp;
+    } catch (e) {
+      state = UserModelError(message: '로그인에 실패했습니다.');
+      return Future.value(state);
+    }
+  }
+
+  Future<void> logout() async {
+    state = null;
+
+    //아래처럼 하면 두개의 키를 차례대로가 아닌 동시에 지워 줄수 있다 >> 둘다 끝날때까지 await
+    await Future.wait(
+      [
+        storage.delete(key: REFRESH_TOKEN_KEY),
+        storage.delete(key: ACCESS_TOKEN_KEY),
+      ],
+    );
   }
 }
